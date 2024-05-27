efabd25e124a01e3e50a49c42a70b64bc1f98e5e
Sebastian Martha <sebas.martha@gmail.com>
2024-05-26 18:20:14 -0600

    Add missing field into register form

--- auth-formulary/frontend/src/presentation/components/auth/register.component.tsx
+++ auth-formulary/frontend/src/presentation/components/auth/register.component.tsx
@@ -22,6 +22,23 @@ const RegisterForm = () => {
         </div>
 
         <div>
+          <label
+            className="block text-sm font-medium leading-6 text-gray-900"
+            htmlFor="username"
+          >
+            Username
+          </label>
+          <div className="mt-2">
+            <input
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              id="username"
+              name="username"
+              type="text"
+            />
+          </div>
+        </div>
+
+        <div>
           <div className="flex items-center justify-between">
             <label
               className="block text-sm font-medium leading-6 text-gray-900"
