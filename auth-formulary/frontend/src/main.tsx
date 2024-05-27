3ab76325fb9a1fa14b2b97458ed61a0730703c6d
Sebastian Martha <sebas.martha@gmail.com>
2024-05-27 14:04:36 -0600

    Add QueryClientProvider configuration for react-query

--- auth-formulary/frontend/src/main.tsx
+++ auth-formulary/frontend/src/main.tsx
@@ -4,11 +4,16 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./application/navigations/router.navigation";
 import "./index.css";
 import Layout from "./presentation/components/layout/layout.component";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Layout>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </Layout>
   </React.StrictMode>
 );
