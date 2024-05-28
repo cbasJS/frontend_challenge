0e722c244237412f86a69cbbc64fc8c94d73dbbd
Sebastian Martha <sebas.martha@gmail.com>
2024-05-27 18:11:55 -0600

    Add RouterNavigation n ReactQueryDevtools component

--- auth-formulary/frontend/src/main.tsx
+++ auth-formulary/frontend/src/main.tsx
@@ -1,19 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./application/navigations/router.navigation";
-import "./index.css";
-import Layout from "./presentation/components/layout/layout.component";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import RouterNavigation from "./application/navigations/router.navigation";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "./index.css";
 
 const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <Layout>
-      <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
-      </QueryClientProvider>
-    </Layout>
-  </React.StrictMode>
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <RouterNavigation />
+      <ReactQueryDevtools initialIsOpen={false} position="bottom" />
+    </QueryClientProvider>
+  </StrictMode>
 );
