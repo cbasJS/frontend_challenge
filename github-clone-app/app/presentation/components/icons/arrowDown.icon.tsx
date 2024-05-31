326aec09eb7debe3f5077d647ed74578c5ddb789
Sebastian Martha <sebas.martha@gmail.com>
2024-05-30 22:55:32 -0600

    Add several icon components

---
+++ github-clone-app/app/presentation/components/icons/arrowDown.icon.tsx
@@ -0,0 +1,11 @@
+import WrapperIcon from "./wrapper.icon";
+
+const ArrowDown = () => {
+  return (
+    <WrapperIcon>
+      <path d="m4.427 7.427 3.396 3.396a.25.25 0 0 0 .354 0l3.396-3.396A.25.25 0 0 0 11.396 7H4.604a.25.25 0 0 0-.177.427Z"></path>
+    </WrapperIcon>
+  );
+};
+
+export default ArrowDown;
