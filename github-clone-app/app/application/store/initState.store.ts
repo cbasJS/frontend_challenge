77e5ed63e73cb806e92a0d2ba45dc35a92dc2b59
Sebastian Martha <sebas.martha@gmail.com>
2024-06-01 14:43:56 -0600

    Add page state into store

--- github-clone-app/app/application/store/initState.store.ts
+++ github-clone-app/app/application/store/initState.store.ts
@@ -2,5 +2,6 @@ import { AppState } from "@/domain/entities/store.entity";
 
 export const defaultInitState: AppState = {
   repositoryType: 'all',
-  isRepositoriesTypeDialogOpen: false
+  isRepositoriesTypeDialogOpen: false,
+  currentPage: "1"
 }
