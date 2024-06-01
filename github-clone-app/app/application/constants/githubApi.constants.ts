cf7daaf7299eb7c79e56d7e96949cafc600444b9
Sebastian Martha <sebas.martha@gmail.com>
2024-06-01 13:32:37 -0600

    Add pageInfo into adapter api response

--- github-clone-app/app/application/constants/githubApi.constants.ts
+++ github-clone-app/app/application/constants/githubApi.constants.ts
@@ -1,4 +1,5 @@
-export const HEADERS: HeadersInit = {
+
+export const HEADERS = {
   'Accept': process.env.NEXT_PUBLIC_GITHUB_API_ACCEPT as string,
   'Authorization': `Bearer ${process.env.NEXT_PUBLIC_GITHUB_TOKEN}`,
   'X-GitHub-Api-Version': process.env.NEXT_PUBLIC_GITHUB_API_VERSION as string
@@ -13,4 +14,11 @@ export const DEFAULT_PARAMS = {
   sort: 'updated',
   per_page: '30',
   direction: 'desc',
+  page: "3"
 }
+
+export const GET_REPOS_LINK = `GET ${process.env.NEXT_PUBLIC_API_URL}/orgs/${process.env.NEXT_PUBLIC_GITHUB_API_ORG}/repos` as string
+
+export const NEXT_PATTERN = /(?<=<)([\S]*)(?=>; rel="next")/i;
+export const LAST_PATTERN = /(?<=<)([\S]*)(?=>; rel="last")/i;
+export const PREV_PATTERN = /(?<=<)([\S]*)(?=>; rel="prev")/i;
--- github-clone-app/app/infrastructure/api/github.api.ts
+++ github-clone-app/app/infrastructure/api/github.api.ts
@@ -1,30 +1,45 @@
-import { GET_OPTIONS, DEFAULT_PARAMS } from "@/application/constants/githubApi.constants";
+import { DEFAULT_PARAMS, GET_REPOS_LINK, HEADERS, NEXT_PATTERN, PREV_PATTERN } from "@/application/constants/githubApi.constants";
+import { getParam } from "@/application/utils/general.util";
 import { repositoryDataAdapter } from "@/domain/adapters/githubApi.adapter";
 import { GithubRepository } from "@/domain/entities/githubApi.entity";
 import { RepositoryTypeString } from "@/domain/enums/githubApi.enum";
+import { Octokit } from "octokit";
+
+const octokit = new Octokit({});
 
 type Params = { type: RepositoryTypeString }
 
 async function getData(params?: Params): Promise<any> {
   try {
-    const paramsToString = new URLSearchParams({
+    const response = await octokit.request(GET_REPOS_LINK, {
       ...DEFAULT_PARAMS,
-      ...params
-    }).toString()
+      ...params,
+      headers: HEADERS,
+    });
 
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_API_URL}/orgs/${process.env.NEXT_PUBLIC_GITHUB_API_ORG}/repos?${paramsToString}`, GET_OPTIONS)
+    const data = response.data || []
 
-    const data = await response.json()
+    const linkHeader = response.headers.link;
 
-    return data;
+    const hasNextPage = linkHeader && linkHeader.includes(`rel=\"next\"`);
+    const nextPageLink = hasNextPage && linkHeader.match(NEXT_PATTERN);
+
+    const hasPrevPage = linkHeader && linkHeader.includes(`rel=\"prev\"`);
+    const prevPageLink = hasPrevPage && linkHeader.match(PREV_PATTERN);
+
+    const pageInfo = {
+      nextPage: getParam(nextPageLink ? nextPageLink[0] : '', 'page'),
+      prevPage: getParam(prevPageLink ? prevPageLink[0] : '', 'page')
+    }
+
+    return { data, pageInfo };
   } catch (e) {
     console.error(e)
-    return []
+    return { data: [], pageInfo: { nextPage: null, prevPage: null } }
   }
 }
 
-export default async function getRepositories(params?: Params): Promise<GithubRepository[]> {
+export default async function getRepositories(params?: Params): Promise<GithubRepository> {
   const data = await getData(params);
   return repositoryDataAdapter(data);
 }
